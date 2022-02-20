6e5c89d0c3799cd7e4442831b28cde1712b72f8c
guitar82 <mikebodell@gmail.com>
2022-02-19 19:51:05 -0500

    Upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,10 +7,10 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
+    siteTitle: `Michael Bodell`,
+    siteTitleAlt: `Michael Bodell - Senior Project Manager`,
+    siteHeadline: `Michael Bodell - Senior Project Manager`,
+    siteUrl: `https://www.mikebodell.com`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
